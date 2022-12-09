1236d60743a3ff39d5e068e33d5f5f064cd9c957
Mavv3006 <52504463+Mavv3006@users.noreply.github.com>
2022-12-09 18:29:30 +0100

    simplyfied RecipeSeeder.php

--- database/seeders/RecipeSeeder.php
+++ database/seeders/RecipeSeeder.php
@@ -12,7 +12,6 @@ use App\Models\Times;
 use App\Models\TimesUnit;
 use App\Models\User;
 use Exception;
-use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
 
 class RecipeSeeder extends Seeder
@@ -25,12 +24,15 @@ class RecipeSeeder extends Seeder
      */
     public function run(): void
     {
-        $user_count = User::all()->count();
-        $has_user = $user_count > 0;
+        if (User::all()->count() == 0) {
+            User::factory()->create();
+        }
+
+        $randomUser = fn() => User::all()->random(1)->first();
 
         $recipes = Recipe::factory()
             ->count(10)
-            ->for($has_user ? User::all()->random(1)->first() : User::factory()->create())
+            ->sequence(fn($sequence) => ['user_id' => $randomUser()])
             ->has(RecipeSteps::factory()->count(4))
             ->hasAttached(
                 Ingredient::factory()->count(3),
@@ -41,30 +43,22 @@ class RecipeSeeder extends Seeder
         foreach ($recipes as $recipe) {
             Comment::factory()
                 ->count(5)
-                ->state(new Sequence(
-                    fn($sequence) => ['user_id' => User::all()->random(1)->first()->id]
-                ))
+                ->sequence(fn($sequence) => ['user_id' => $randomUser()->id])
                 ->create(['recipe_id' => $recipe->id]);
 
             $rating_count = 5;
             $users = User::all()->random($rating_count);
             Rating::factory()
                 ->count($rating_count)
-                ->state(new Sequence(
-                    fn($sequence) => ['user_id' => $users[$sequence->index]]
-                ))
+                ->sequence(fn($sequence) => ['user_id' => $users[$sequence->index]])
                 ->create(['recipe_id' => $recipe->id]);
 
             $times_count = 3;
             $times = Times::all()->random($times_count);
             RecipeTimes::factory()
                 ->count($times_count)
-                ->state(new Sequence(
-                    fn($sequence) => ['times_id' => $times[$sequence->index]]
-                ))
-                ->state(new Sequence(
-                    fn($sequence) => ['times_unit_id' => TimesUnit::all()->random(1)->first()->id]
-                ))
+                ->sequence(fn($sequence) => ['times_id' => $times[$sequence->index]])
+                ->sequence(fn($sequence) => ['times_unit_id' => TimesUnit::all()->random(1)->first()->id])
                 ->create(['recipe_id' => $recipe->id]);
         }
     }
